2f0c32ed2dc12b262a7fcd29b77e1dcd58100065
Subahu Rayamajhi <subahur@brandeis.edu>
2015-02-26 21:05:45 -0500

    stock scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :stocks
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227020132) do
+ActiveRecord::Schema.define(version: 20150227020520) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string   "stock_name"
+    t.string   "stock_symbol"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "user_name"
