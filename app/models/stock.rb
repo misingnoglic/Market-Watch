9e9e3c5aba4d5bc2809ae3c8cd60b26d08711f7a
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-02-28 18:47:20 -0500

    finished relationships

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,4 +2,5 @@ class Stock < ActiveRecord::Base
     has_many :portfolios
     has_many :watchlist
     has_many :users, through: :portfolios
+    has_many :users, through: :watchlist
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
     has_many :portfolios
+    has_many :watchlists
     has_many :stocks, through: :portfolios
+    has_many :stocks, through: :watchlists
 end
