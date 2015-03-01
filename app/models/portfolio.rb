fdb45afafc15e143a058faac48b961eee4f32274
Subahu Rayamajhi <subahur@brandeis.edu>
2015-02-28 19:35:49 -0500

    Portfolio and user table validations

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ActiveRecord::Base
+	validates :number_shares, presence: true, numericality: { :greater_than_or_equal_to => 1 }, message: "Number of shares must be entered"
+    
     belongs_to :user
     belongs_to :stock
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :user_name, :password, :email, presence: true
+	validates :email, uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX}
+	validates :password, length: { in: 6..20 } 
+
     has_many :portfolios
     has_many :watchlists
     has_many :stocks, through: :portfolios
     has_many :stocks, through: :watchlists
 end
+
+
