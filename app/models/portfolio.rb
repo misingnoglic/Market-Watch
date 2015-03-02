f9acb5095c5bfddbbfdd9e12cf525e53fd2c3526
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-01 21:28:02 -0500

    validator change in portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,5 @@
 class Portfolio < ActiveRecord::Base
-	validates :number_shares, presence: true, numericality: { :greater_than_or_equal_to => 1 }, message: "Number of shares must be entered"
+	validates :number_shares, presence: true, numericality: { :greater_than_or_equal_to => 1 }
     
     belongs_to :user
     belongs_to :stock
