94a024ddc26c65b082b8e0e799f6b15b285c8a8e
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-03-01 21:50:53 -0500

    unit test for user model validation- email uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,12 +4,21 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-    test "user attributes must not be empty" do
-        user = User.new
-        assert user.invalid?
-        assert user.errors[:user_name].any?
-        assert user.errors[:password].any?
-        assert user.errors[:email].any?
-    end
+  test "user attributes must not be empty" do
+      user = User.new
+      assert user.invalid?
+      assert user.errors[:user_name].any?
+      assert user.errors[:password].any?
+      assert user.errors[:email].any?
+  end
+
+  test "user is not valid without a unique email" do
+    user = User.new(email: users(:one).email,
+    user_name: "yyy",
+    password: "hahahahahah")
+    assert !user.save
+    assert_equal "has already been taken", user.errors[:email].join('; ') 
+  end
+
   
 end
