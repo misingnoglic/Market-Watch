c99e5711f653a4c2355a86c62ac4e19bf58d7cbc
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-03-01 21:27:36 -0500

    unit test for validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+    test "user attributes must not be empty" do
+        user = User.new
+        assert user.invalid?
+        assert user.errors[:user_name].any?
+        assert user.errors[:password].any?
+        assert user.errors[:email].any?
+    end
+  
 end
