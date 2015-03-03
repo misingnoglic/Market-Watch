3368e5690fff8586721b5c537af00b04816ac78e
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-02 19:39:28 -0500

    Revert "changed password to password_digest in user model"
    This reverts commit df91a7bf6b9e75442f39625530162656453e919b.

--- db/migrate/20150227020132_create_users.rb
+++ db/migrate/20150227020132_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :user_name
-      t.string :password_digest
+      t.string :password
       t.string :email
 
       t.timestamps null: false
