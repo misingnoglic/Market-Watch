2a049ca47f91c5b5f6225689127a266c9892e228
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-03-02 22:45:02 -0500

    test for 14.1 updated on test/controller/user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
+    @input_attributes = {
+    name: "sam", password: "private", password_confirmation: "private"}
     @user = users(:one)
   end
 
