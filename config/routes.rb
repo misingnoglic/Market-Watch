571770f2d70f19847433acff58da7b47b038d338
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-02 23:31:05 -0500

    Chapter 14.2: Authenticating users
    This is team member 2 part for toms homework

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create 
+    delete 'logout' => :destroy
+  end
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :portfolios
 
