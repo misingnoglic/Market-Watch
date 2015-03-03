df91a7bf6b9e75442f39625530162656453e919b
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-02 19:32:28 -0500

    changed password to password_digest in user model

--- db/migrate/20150227020132_create_users.rb
+++ db/migrate/20150227020132_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :user_name
-      t.string :password
+      t.string :password_digest
       t.string :email
 
       t.timestamps null: false
