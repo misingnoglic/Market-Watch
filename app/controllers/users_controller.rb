8ceba99d062c2da447ece931ae7063b1970fb736
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-03-02 22:18:35 -0500

    updated 14.1 from the book, users model,view and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,11 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+      @users = User.order(:user_name)
+      respond_to do |format|
+          format.html
+          format.json {render json: @users }
+      end
   end
 
   # GET /users/1
@@ -24,14 +28,14 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+      @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to users_url, notice: 'User #{@user.user_name} was successfully created.' }
+        format.json { render @user, status: :created, location: @user }
       else
-        format.html { render :new }
+        format.html { render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -40,12 +44,13 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+      @user = User.find(params[:id])
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+      if @user.update_attributes(user_params)
+        format.html { redirect_to users_url, notice: 'User #{@user.user_name} was successfully updated.' }
+        format.json { head:no_content }
       else
-        format.html { render :edit }
+        format.html { render action: "edit" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :user_name, :password_digest, :email, presence: true
 	validates :email, uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX}
-	validates :password_digest, length: { in: 6..20 } 
+    #validates :password_digest, length: { in: 6..20 }
+    has_secure_password
 
     has_many :portfolios
     has_many :watchlists
