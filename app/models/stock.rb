95ae8abe037a25ca8bebb94a391b49eda1b78534
arya <aboudaie@brandeis.edu>
2015-03-04 00:56:31 -0500

    Added change from yesterday to stock field

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -10,6 +10,10 @@ class Stock < ActiveRecord::Base
   def price
       YahooFinance::get_quotes( YahooFinance::StandardQuote, self.stock_symbol )[self.stock_symbol.upcase].lastTrade
   end
+
+  def historical_price(days_ago)
+    (YahooFinance::get_historical_quotes_days( self.stock_symbol, days_ago ).last.last.to_f)
+  end
 end
 
 
