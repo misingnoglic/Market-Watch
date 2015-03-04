dfb9ba8bc9669e6773d1ac0098151ea9d13a70ee
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-04 12:00:23 -0500

    db migrate update..join table
    there were some problems in the db_migrate/create_portfolio
    2 rows needed to be added for the associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303014121) do
+ActiveRecord::Schema.define(version: 20150304163158) do
 
   create_table "portfolios", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "stock_id"
     t.integer  "number_shares"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  add_index "portfolios", ["stock_id"], name: "index_portfolios_on_stock_id"
+  add_index "portfolios", ["user_id"], name: "index_portfolios_on_user_id"
 
   create_table "stocks", force: :cascade do |t|
     t.string   "stock_name"
