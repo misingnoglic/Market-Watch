2a3a4c8a97cded4b9bfa31db595c212f34261fe2
arya <aboudaie@brandeis.edu>
2015-03-04 13:47:40 -0500

    Added login, rounded price change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authorize
+  protected
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, notice: "Please log in" end
+  end
+
 end
