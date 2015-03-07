aa02d55996a5178c83b387d14a93fd983d4c4a49
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-07 14:00:23 -0500

    revision in routes.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ def create
 	user = User.find_by_user_name(params[:user_name])
 if user and user.authenticate(params[:password])
     session[:user_id] = user.id
-    redirect_to admin_url
 else
 	redirect_to login_url, alert: "Invalid user/password combination"
 	end
