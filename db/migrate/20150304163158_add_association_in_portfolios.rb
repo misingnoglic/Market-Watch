8a88c21e4d19e15a8ed9851a0822a7ce8c7605cd
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-18 14:53:12 -0400

    Revert cf21f13..f3d89a1
    This rolls back to commit cf21f1380d187b2881f9f8c9ccd74753b906c736.

--- db/migrate/20150304163158_add_association_in_portfolios.rb
+++ db/migrate/20150304163158_add_association_in_portfolios.rb
@@ -2,10 +2,10 @@ class AddAssociationInPortfolios < ActiveRecord::Migration
   def change
   	drop_table :portfolios
 
-  	create_table :portfolios do |t|
-      	  t.belongs_to :user, index: true
-     	  t.belongs_to :stock, index: true
-          t.integer :number_shares
+  	 create_table :portfolios do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :stock, index: true
+      t.integer :number_shares
       
       t.timestamps null: false
     end
