f468567aa36fd133fe5aa567850f1f53e1cb25fb
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-18 15:22:20 -0400

    Final new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  skip_before_filter :authorize, only: [:new]
+  skip_before_filter :authorize, only: [:new, :create]
   # GET /users
   # GET /users.json
   def index
