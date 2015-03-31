f1e1e4691e667cd6073aefee55c854df37dfcb8a
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-30 22:55:59 -0400

    logout button in portfolios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330044802) do
+ActiveRecord::Schema.define(version: 20150330030844) do
 
   create_table "line_items", force: :cascade do |t|
     t.integer  "stock_id"
@@ -51,11 +51,6 @@ ActiveRecord::Schema.define(version: 20150330044802) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.boolean  "admin",           default: false
-  end
-
-  create_table "watchlists", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
