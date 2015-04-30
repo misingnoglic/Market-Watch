52fa7a4a45072240a8dcf96b51d4250313c586c2
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-04-30 18:17:13 -0400

    Revert beb09a9..dfe912d
    This rolls back to commit beb09a9b3b210ed81a3f81064e1cf08f24ccd579.

--- config/initializers/update_start.rb
+++ config/initializers/update_start.rb
@@ -1,8 +1,6 @@
  require 'update_stocks_job'
  require 'update_tweets_job'
 
-#update stocks, stock history and tweets
-StockHistory.destroy_all
+
 ::UpdateJob.new.async.perform("asdf")
 ::UpdateTweetsJob.new.async.perform("asdf")
-
