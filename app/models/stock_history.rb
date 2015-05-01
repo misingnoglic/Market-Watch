f732232d08a3ed005aa4b78b61d0815da830e016
arya <aboudaie@brandeis.edu>
2015-05-01 14:57:40 -0400

    Added method to create JSON of stock histories

--- app/models/stock_history.rb
+++ app/models/stock_history.rb
@@ -1,2 +1,13 @@
 class StockHistory < ActiveRecord::Base
+  require 'json'
+
+  def createHighchartsJSON(symbol)
+    stock_histories = StockHistory.where(stock_symbol: symbol)
+    histories = []
+    for stock_history in stock_histories do
+      histories.append([stock_history.created_at.utc.to_i, stock_history.last_trade_price])
+    end
+    return JSON.generate(histories)
+  end
+
 end
