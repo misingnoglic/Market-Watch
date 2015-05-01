f27a75dbbd9d419a099c95db0a032a609786e9dd
Subahu Rayamajhi <subahur@brandeis.edu>
2015-05-01 02:08:03 -0400

    comparator added to rules form

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -88,6 +88,6 @@ class RulesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def rule_params
-      params.require(:rule).permit(:target_price, :user_id, :stock_id, :stock_symbol, :lastfired)
+      params.require(:rule).permit(:target_price, :user_id, :stock_id, :stock_symbol, :lastfired, :compare)
     end
 end
