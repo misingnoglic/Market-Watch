9cbad2aa9baacfbc956b671d6106c54e4fbe7075
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-04-30 20:36:36 -0400

    small change, added $ in front of stock symbol to search tweets

--- lib/update_tweets.rb
+++ lib/update_tweets.rb
@@ -16,7 +16,7 @@ class UpdateTweets
     Tweet.destroy_all
     @stocks = Stock.all
     @stocks.each do |stock| 
-      @client.search("#{stock.stock_symbol} since:#{Date.yesterday()}", result_type: "popular").take(3).collect do |tweet| 
+      @client.search("$#{stock.stock_symbol} since:#{Date.yesterday()}", result_type: "popular").take(3).collect do |tweet| 
 			 analyzer = Sentimental.new
        Tweet.create(
 			   	:screen_name => tweet.user.screen_name,
