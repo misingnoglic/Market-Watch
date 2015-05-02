22ad8eee18ee803770a0d05be42f390e671c5e41
Subahu Rayamajhi <subahur@brandeis.edu>
2015-05-02 19:16:58 -0400

    portfolio sorted by stock name

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,7 +2,8 @@ class LineItem < ActiveRecord::Base
     belongs_to :stock
     belongs_to :portfolio
         validate :check_unique #Warning- Validation will still pass in cases when stock_ids are equal and symbols are equal
-        
+    scope :ordered_by_stock_name, -> { joins(:stock).order('stocks.stock_name') }
+
     def check_unique
          curr_portfolio = Portfolio.find(portfolio_id)
          curr_stock = Stock.find (stock_id)
