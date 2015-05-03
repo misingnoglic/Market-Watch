6fac75cbc2ec98ee06df67178c1c0bd9fd80ba44
arya <aboudaie@brandeis.edu>
2015-05-03 17:02:08 -0400

    Adds the rule to the stock chart, charts now created in chart model

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -16,6 +16,16 @@ class Chart < ActiveRecord::Base
     return JSON.generate(time_v_price)
   end
 
+  def self.get_API_history_json (stock_symbol, days_ago)
+    prices = (YahooFinance::get_historical_quotes_days( stock_symbol, days_ago ))
+    returned_list = []
+    prices.each do |price_list|
+      returned_list.push([price_list.first,price_list.last.to_f])
+    end
+    returned_list = returned_list.reverse
+    return JSON.generate([['Date','Price']]+returned_list)
+  end
+
   def self.get_sentiment_history_json (stock_symbol)
     sentiment_histories = Stock.by_stock_symbol(stock_symbol)
     
@@ -33,8 +43,20 @@ class Chart < ActiveRecord::Base
     (old_chart.length - 1).times do |index|
       old_chart[index+1]+=[target_price]
     end
+    return JSON.generate(old_chart)
   end
-  return JSON.generate(old_chart)
+
+  def self.create_chart_data (stock_symbol)
+    id = Stock.where(stock_symbol: stock_symbol)[0].id
+    rules = Rule.where(stock_id: id)
+    old_chart = get_API_history_json(stock_symbol, 7)
+    for rule in rules.each do
+
+      old_chart = self.add_target_to_json(old_chart,rule.target_price, "target")
+    end
+    return old_chart
+  end
+
 end
 
   
