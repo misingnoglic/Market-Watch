af43254e397cd7ef24f746b7f5838a4d651b40f6
Subahu Rayamajhi <subahur@brandeis.edu>
2015-05-02 23:41:28 -0400

    redirect to line item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -71,7 +71,7 @@ class LineItemsController < ApplicationController
     curr_portfolio = @line_item.portfolio
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to curr_portfolio, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
