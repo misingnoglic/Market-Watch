22ce934a7804475973a455eece05c999af07e370
Subahu Rayamajhi <subahur@brandeis.edu>
2015-05-05 03:47:18 -0400

    Sentiment rules working properly

--- app/models/rules/sentiment_rule.rb
+++ app/models/rules/sentiment_rule.rb
@@ -3,24 +3,43 @@ class SentimentRule < Rule
 
 
 	def checkTrigger
-          if target <= stock.sentiment_score  #should the comparator be a seperate function to prevent seperate class for each for >, >= <= etc ??? 
-            if canfire? 
-            myobj = Rule.find(self.id)
-            myobj[:last_fired] = Time.now.to_i
-            myobj.save!
-            Rails.logger.debug "goo #{myobj.attributes.inspect}"
-             notification = self.notifications.create
-             RuleMailer.sample_email(self).deliver_now
+        if self.compare==1
+            if target <= stock.sentiment_score  #should the comparator be a seperate function to prevent seperate class for each for >, >= <= etc ??? 
+                if canfire? 
+                myobj = Rule.find(self.id)
+                myobj[:last_fired] = Time.now.to_i
+                myobj.save!
+                Rails.logger.debug "goo #{myobj.attributes.inspect}"
+                notification = self.notifications.create
+                RuleMailer.sample_email(self).deliver_now
+                end
             end
-          end
+        else
+            if target >= stock.sentiment_score  #should the comparator be a seperate function to prevent seperate class for each for >, >= <= etc ??? 
+                if canfire? 
+                myobj = Rule.find(self.id)
+                myobj[:last_fired] = Time.now.to_i
+                myobj.save!
+                Rails.logger.debug "goo #{myobj.attributes.inspect}"
+                notification = self.notifications.create
+                RuleMailer.sample_email(self).deliver_now
+                end
+            end 
+        end
 	end
      
     def target
         return self.target_price
     end
 
-     def message
-           return " has reached the sentiment_score of "
+
+
+    def message
+        if target <= stock.sentiment_score
+            return " sentiment score has reached the target score of "
+        else
+            return " last sentiment score has dropped below "
         end
+    end
 
 end
