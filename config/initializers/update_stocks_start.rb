b03b634029f9e4f8713dde0f45a40979e02cc196
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-05-05 17:31:31 -0400

    try1

--- config/initializers/update_stocks_start.rb
+++ config/initializers/update_stocks_start.rb
@@ -1,4 +1,11 @@
  require 'update_stocks_job'
 
+require 'feedjira'
+require 'update_rss_feed_job'
+
 ::UpdateJob.new.async.perform("asdf")
 
+
+
+FeedEntry.get_feed
+::UpdateFeedJob.new.async.perform("asdf")
