1f2a5c1421f498216436d05098565fc916c2864d
Subahu Rayamajhi <subahur@brandeis.edu>
2015-05-05 19:26:12 -0400

    variable name correction

--- app/models/rules/last_price_rule.rb
+++ app/models/rules/last_price_rule.rb
@@ -1,7 +1,7 @@
 class LastPriceRule < Rule
 
 	def checkTrigger
-          if self.compare == "greater"
+          if self.compare_type == "greater"
             if target <= stock.last_trade_price  #should the comparator be a seperate function to prevent seperate class for each for >, >= <= etc ??? 
               if canfire? 
               myobj = Rule.find(self.id)
