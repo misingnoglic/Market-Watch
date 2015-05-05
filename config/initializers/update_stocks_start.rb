00c184464b859233067b6a3b36d74f8f659513fd
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-05-05 17:32:37 -0400

    Revert "try1"
    This reverts commit b03b634029f9e4f8713dde0f45a40979e02cc196.

--- config/initializers/update_stocks_start.rb
+++ config/initializers/update_stocks_start.rb
@@ -1,11 +1,4 @@
  require 'update_stocks_job'
 
-require 'feedjira'
-require 'update_rss_feed_job'
-
 ::UpdateJob.new.async.perform("asdf")
 
-
-
-FeedEntry.get_feed
-::UpdateFeedJob.new.async.perform("asdf")
