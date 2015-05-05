e231ee5fdbf3fa091045998b6450a479588efdfa
arya <aboudaie@brandeis.edu>
2015-05-05 16:28:33 -0400

    commented file

--- config/initializers/zupdate_feed_start.rb
+++ config/initializers/zupdate_feed_start.rb
@@ -1,7 +1,7 @@
-require 'feedjira'
-require 'update_rss_feed_job'
+#require 'feedjira'
+#require 'update_rss_feed_job'
 
 
-FeedEntry.get_feed
-::UpdateFeedJob.new.async.perform("asdf")
+#FeedEntry.get_feed
+#::UpdateFeedJob.new.async.perform("asdf")
     
