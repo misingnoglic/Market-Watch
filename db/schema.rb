47b61a17ec0abfbd90a71578ebf48a37294fcbd5
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-05-06 00:02:32 -0400

    Tweets done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505222451) do
+ActiveRecord::Schema.define(version: 20150506034441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20150505222451) do
     t.integer  "stock_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.datetime "tweeted_at"
   end
 
   create_table "users", force: :cascade do |t|
