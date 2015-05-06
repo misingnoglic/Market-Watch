2fdd328e5dbb5f4d14e0a42e7aa1d66bf4c279cc
dfit99 <dfit99@brandeis.edu>
2015-05-06 18:28:30 -0400

    primitive stock test

--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class StockTest < ActiveSupport::TestCase
  
   test 'is created' do
-   #stock= @stocks['Google']['GOOG']
-  # assert_equal stock[:stock_symbol], 'GOOG' 
-  assert_equal 5, 5
+     
+  assert_equal 'GOOG', stocks(:Google).stock_symbol
   end
 end
